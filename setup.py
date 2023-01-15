adcd701ecc75bd916b6183ad5885216c52e1c079
Connor DeLong <condelong11@yahoo.com>
2023-01-15 11:20:27 -0500

    added files to package the project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="UD_draft_model",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=requirements
+)
