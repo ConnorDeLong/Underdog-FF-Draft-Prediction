42bfab412198728af8f2474496d416e0d0e92809
Connor DeLong <condelong11@yahoo.com>
2023-09-02 13:54:51 -0400

    removed inference directory

--- UD_draft_model/inference/test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-import getpass
-
-import UD_draft_model.scrapers.scrape_site.scrape_league_data as scrape_site
-import UD_draft_model.scrapers.scrape_site.pull_bearer_token as pb
